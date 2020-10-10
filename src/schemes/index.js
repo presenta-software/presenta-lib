ce9c293e3e4a94c49c83b6b60ebf785c9745b82b
abusedmedia <ff@todo.to.it>
2020-10-10 19:11:41 +0200

    exposed scheme list

---
+++ src/schemes/index.js
@@ -0,0 +1,13 @@
+import './original.css'
+import './vibrant.css'
+import './polite.css'
+import './breeze.css'
+import './sunny.css'
+import './acid.css'
+import './novel.css'
+import './doub.css'
+import './mayb.css'
+
+const schemes = ['original', 'vibrant', 'polite', 'breeze', 'sunny', 'acid', 'novel', 'doub', 'mayb']
+
+export default schemes

734d0fd6eec4fec55b0adeb1af1d40821b1909fa
abusedmedia <ff@todo.to.it>
2020-10-11 15:02:13 +0200

    added two more controllers

--- src/router/controllers/autoplay/index.js
+++ src/router/controllers/autoplay/index.js
@@ -1,6 +1,5 @@
 const autoplay = function (rootElement, router, ctrlConfig, projectConfig) {
   let timer = null
-  const loop = !ctrlConfig.noloop
   const defdelay = ctrlConfig.delay || 4000
   let lastdelay = 0
 
@@ -12,16 +11,9 @@ const autoplay = function (rootElement, router, ctrlConfig, projectConfig) {
     }, delay)
   }
 
-  router.on('end', evt => {
-    if (loop) {
-      router.goto(0)
-      router.notify('nextIndex')
-    }
-  })
-
   router.on('indexChanged', evt => {
-    const cScene = router.projectConfig.scenes[evt.currentIndex]
-    const delay = cScene.duration || defdelay
+    const cScene = projectConfig.scenes[evt.currentIndex]
+    const delay = cScene.autoplayDuration || defdelay
     runTime(delay)
   })
 }

1923f3a7511d4fb276ca68e518b6b6b05e809b07
abusedmedia <ff@todo.to.it>
2020-10-13 19:05:15 +0200

    added fontkits

---
+++ src/fontkits/index.js
@@ -0,0 +1,11 @@
+import './fk1.css'
+import './fk2.css'
+import './fk3.css'
+import './fk4.css'
+import './fk5.css'
+import './fk6.css'
+import './fk7.css'
+
+const fontkits = ['fk1', 'fk2', 'fk3', 'fk4', 'fk5', 'fk6', 'fk7']
+
+export default fontkits

caf4c9810dd064de6412cd8ac15fe139c3a82e8d
abusedmedia <ff@todo.to.it>
2020-10-24 17:01:10 +0200

    added hidden controller

---
+++ src/controllers/hidden/index.js
@@ -0,0 +1,27 @@
+
+const hidden = function (rootElement, router, ctrlConfig, projectConfig) {
+  const scenesToRemove = []
+  const scenes = projectConfig.scenes
+  scenes.forEach((scene, i) => {
+    const blocks = scene.blocks
+    const blocksToRemove = []
+
+    if (scene.hidden) {
+      scenesToRemove.push(i)
+    } else {
+      blocks.forEach((block, j) => {
+        if (block.hidden) blocksToRemove.push(j)
+      })
+    }
+
+    for (var j = blocks.length - 1; j >= 0; j--) {
+      if (blocksToRemove.indexOf(j) >= 0) blocks.splice(j, 1)
+    }
+  })
+
+  for (var i = scenes.length - 1; i >= 0; i--) {
+    if (scenesToRemove.indexOf(i) >= 0) scenes.splice(i, 1)
+  }
+}
+
+export { hidden }

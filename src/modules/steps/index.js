a7dafadb7e56c6025a0119c119d441230ce49362
abusedmedia <ff@todo.to.it>
2020-11-11 21:31:20 +0100

    better steps module

--- src/modules/steps/index.js
+++ src/modules/steps/index.js
@@ -1,17 +1,33 @@
 import css from './style.css'
 
 const steps = function (sceneElement, modConfig, sceneConfig) {
+  if (sceneConfig._mode !== 'present') return
+
   let allStepElements = []
+  const defaultSteps = typeof modConfig === 'string' ? modConfig : '.step'
 
   let index = 0
-  sceneConfig.blocks.forEach(b => {
+  const blocks = sceneConfig.blocks.filter(b => {
+    if (b.hasOwnProperty('steps') && !b.steps) {
+      return false
+    } else {
+      return true
+    }
+  })
+
+  blocks.forEach(b => {
     const blockEl = b._el
-    const tag = typeof b.step === 'string' ? b.step : '.step'
+    const tag = typeof b.steps === 'string' ? b.steps : defaultSteps
+    const tags = tag.split(',')
 
-    const blockStepElements = [].slice.call(blockEl.querySelectorAll(tag))
+    let blockStepElements = []
 
-    blockStepElements.sort((a, b) => {
-      return a.dataset.order - b.dataset.order
+    tags.forEach(tg => {
+      const elms = [].slice.call(blockEl.querySelectorAll(tg))
+      elms.sort((a, b) => {
+        return a.dataset.order - b.dataset.order
+      })
+      blockStepElements = blockStepElements.concat(elms)
     })
 
     blockStepElements.forEach(el => {

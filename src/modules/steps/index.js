5aed656b33e147f758ed7ffc449d77df61276e6d
abusedmedia <ff@todo.to.it>
2020-11-27 15:10:20 +0100

    steps module refactoring and bug fixes

--- src/modules/steps/index.js
+++ src/modules/steps/index.js
@@ -1,19 +1,51 @@
 import css from './style.css'
 
+const validTrans = ['fadeIn', 'zoomOut', 'zoomIn', 'slideUp', 'slideDown']
+const validModes = ['sequential', 'match']
+
+const parseSettings = cnf => {
+  let tag = null
+  let trans = null
+  let mode = null
+
+  if (typeof cnf === 'string') {
+    tag = cnf
+  }
+  if (typeof cnf === 'object') {
+    tag = cnf.selector
+    trans = cnf.transition
+    mode = cnf.mode
+  }
+  return { tag, trans, mode }
+}
+
 const steps = function (sceneElement, modConfig, sceneConfig) {
   if (sceneConfig._mode !== 'present') return
 
-  const sceneMode = sceneConfig.steps || 'sequential'
+  const modSett = parseSettings(modConfig)
+  let defTag = modSett.tag || '.step'
+  let defTrans = modSett.trans || validTrans[0]
+  const defMode = modSett.mode || validModes[0]
 
-  const defaultSteps = typeof modConfig === 'string' ? modConfig : '.step'
+  const sceneSett = parseSettings(sceneConfig.steps)
+  let sceneMode = sceneSett.mode || defMode
+  if (validModes.indexOf(sceneMode) === -1) sceneMode = validModes[0]
 
-  const allElems = {}
+  defTag = sceneSett.tag || defTag
+  defTrans = sceneSett.trans || defTrans
+
   let index = 0
+  const allElems = {}
   const blocks = sceneConfig.blocks.filter(b => !(b.hasOwnProperty('steps') && !b.steps))
 
   blocks.forEach(b => {
     const blockEl = b._el
-    const tag = typeof b.steps === 'string' ? b.steps : defaultSteps
+
+    const sett = parseSettings(b.steps)
+    const tag = sett.tag || defTag
+    let trans = sett.trans || defTrans
+    if (validTrans.indexOf(trans) === -1) trans = validTrans[0]
+
     const tags = tag.split(',')
 
     const blockStepElements = []
@@ -38,11 +70,12 @@ const steps = function (sceneElement, modConfig, sceneConfig) {
       blockStepElements.forEach(ob => {
         const els = ob.els
         els.forEach(el => {
-          el.classList.add(css.step, css.initState)
+          el.classList.add(css.step, css[trans])
 
           const id = {
             sandbox: 'steps',
             index,
+            trans,
             els: [el]
           }
           sceneConfig._steps.push(id)
@@ -58,21 +91,25 @@ const steps = function (sceneElement, modConfig, sceneConfig) {
         const els = ob.els
 
         if (!allElems[sel]) {
-          allElems[sel] = els
+          allElems[sel] = { arr: els, trans }
         } else {
-          allElems[sel] = allElems[sel].concat(els)
+          allElems[sel].arr = allElems[sel].arr.concat(els)
         }
       })
     }
+
+    // end blocks
   })
 
   for (const k in allElems) {
-    allElems[k].forEach(el => (el.classList.add(css.step, css.initState)))
+    const trans = allElems[k].trans
+    allElems[k].arr.forEach(el => (el.classList.add(css.step, css[trans])))
 
     const id = {
       sandbox: 'steps',
       index,
-      els: allElems[k]
+      trans,
+      els: allElems[k].arr
     }
     sceneConfig._steps.push(id)
 
@@ -82,7 +119,7 @@ const steps = function (sceneElement, modConfig, sceneConfig) {
   this.stepForward = step => {
     if (step.sandbox === 'steps') {
       const els = step.els
-      els.forEach(el => (el.classList.remove(css.initState)))
+      els.forEach(el => (el.classList.remove(css[step.trans])))
     }
   }
 }

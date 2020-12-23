db3bbf4cf86313286241b0827d9e541c97ebdac0
abusedmedia <ff@todo.to.it>
2020-12-23 10:57:06 +0100

    initial brand ctrl dev

---
+++ src/controllers/brand/index.js
@@ -0,0 +1,40 @@
+import './globals.css'
+import css from './style.css'
+import u from '../../utils.js'
+
+u.addProp([
+  'brandPadding',
+  'brandInnerPadding',
+  'brandBackColor',
+  'brandColor',
+  'brandBorder',
+  'brandBorderRadius',
+  'brandFlexAlign',
+  'brandFlexJustify'
+])
+u.addGlob(['brandPosition'])
+
+const brand = function (rootElement, router, ctrlConfig, projectConfig) {
+  let timer = null
+  const child = u.div(`<div class="${css.brand}"></div>`)
+  const inner = u.div(`<div class="${css.inner}"></div>`)
+  const content = u.div(`<div class="${css.content}"></div>`)
+  inner.appendChild(content)
+  child.appendChild(inner)
+  rootElement.appendChild(child)
+
+  content.innerHTML = '<h1>BRAND</h1>'
+
+  rootElement.parentNode.addEventListener('mousemove', e => {
+    scheduleForHide()
+  })
+  const scheduleForHide = () => {
+    clearTimeout(timer)
+    child.classList.remove(css.hide)
+    timer = setTimeout(() => {
+      child.classList.add(css.hide)
+    }, 3000)
+  }
+}
+
+export { brand }

dbfe2f95f6c8fbcd86779891695feaea631c6beb
abusedmedia <ff@todo.to.it>
2020-12-23 10:56:47 +0100

    refined minitools ctrl

--- src/controllers/minitools/index.js
+++ src/controllers/minitools/index.js
@@ -2,6 +2,13 @@ import './globals.css'
 import css from './style.css'
 import u from '../../utils.js'
 
+import fullscreen from './icons/maximize-2.svg'
+import twitter from './icons/twitter.svg'
+import facebook from './icons/facebook.svg'
+import github from './icons/github.svg'
+import linkedin from './icons/linkedin.svg'
+import home from './icons/home.svg'
+
 u.addProp([
   'minitoolsPadding',
   'minitoolsInnerPadding',
@@ -12,9 +19,38 @@ u.addProp([
   'minitoolsFlexAlign',
   'minitoolsFlexJustify'
 ])
-u.addGlob(['minitoolsPosition'])
+u.addGlob(['minitoolsPosition', 'minitoolsDirection'])
+
+const options = {
+  fullscreen: {
+    active: true,
+    icon: fullscreen,
+    action: () => (console.log('clicked'))
+  },
+  twitter: {
+    active: false,
+    icon: twitter
+  },
+  facebook: {
+    active: false,
+    icon: facebook
+  },
+  github: {
+    active: false,
+    icon: github
+  },
+  linkedin: {
+    active: false,
+    icon: linkedin
+  },
+  home: {
+    active: false,
+    icon: home
+  }
+}
 
 const minitools = function (rootElement, router, ctrlConfig, projectConfig) {
+  let timer = null
   const child = u.div(`<div class="${css.minitools}"></div>`)
   const inner = u.div(`<div class="${css.inner}"></div>`)
   const content = u.div(`<div class="${css.content}"></div>`)
@@ -22,12 +58,35 @@ const minitools = function (rootElement, router, ctrlConfig, projectConfig) {
   child.appendChild(inner)
   rootElement.appendChild(child)
 
-  /*
-  fullscreen
-  socials
-  web
-  toggle navs?
-  */
+  for (const k in options) {
+    const opt = options[k]
+    const c = ctrlConfig[k]
+    if (c || opt.active) {
+      const svg = u.div(`<div class="${css.icon}" title="${k}">
+        <a href="${c}" target="blank">${opt.icon}</a>
+      </div>`)
+      content.appendChild(svg)
+
+      if (opt.action) {
+        svg.addEventListener('click', e => {
+          opt.action()
+          e.preventDefault()
+          e.stopPropagation()
+        })
+      }
+    }
+  }
+
+  rootElement.parentNode.addEventListener('mousemove', e => {
+    scheduleForHide()
+  })
+  const scheduleForHide = () => {
+    clearTimeout(timer)
+    child.classList.remove(css.hide)
+    timer = setTimeout(() => {
+      child.classList.add(css.hide)
+    }, 3000)
+  }
 }
 
 export { minitools }

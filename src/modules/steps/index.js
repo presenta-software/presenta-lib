9387e9ce27125c3c2ae9cc06327f0a3a82ada0f3
abusedmedia <ff@todo.to.it>
2020-12-26 15:19:15 +0100

    added out option in steps

--- src/modules/steps/index.js
+++ src/modules/steps/index.js
@@ -7,6 +7,7 @@ const parseSettings = cnf => {
   let tag = null
   let trans = null
   let mode = null
+  let out = null
 
   if (typeof cnf === 'string') {
     tag = cnf
@@ -15,8 +16,9 @@ const parseSettings = cnf => {
     tag = cnf.selector
     trans = cnf.transition
     mode = cnf.mode
+    out = cnf.out
   }
-  return { tag, trans, mode }
+  return { tag, trans, mode, out }
 }
 
 const steps = function (sceneElement, modConfig, sceneConfig) {
@@ -24,6 +26,7 @@ const steps = function (sceneElement, modConfig, sceneConfig) {
 
   const modSett = parseSettings(modConfig)
   let defTag = modSett.tag || '.step'
+  const defOut = modSett.out || false
   let defTrans = modSett.trans || validTrans[0]
   const defMode = modSett.mode || validModes[0]
 
@@ -37,12 +40,14 @@ const steps = function (sceneElement, modConfig, sceneConfig) {
   let index = 0
   const allElems = {}
   const blocks = sceneConfig.blocks.filter(b => !(b.hasOwnProperty('steps') && !b.steps))
+  let prevEls = null
 
   blocks.forEach(b => {
     const blockEl = b._el
 
     const sett = parseSettings(b.steps)
     const tag = sett.tag || defTag
+    const out = sett.out || defOut
     let trans = sett.trans || defTrans
     if (validTrans.indexOf(trans) === -1) trans = validTrans[0]
 
@@ -76,6 +81,7 @@ const steps = function (sceneElement, modConfig, sceneConfig) {
             sandbox: 'steps',
             index,
             trans,
+            out,
             els: [el]
           }
           sceneConfig._steps.push(id)
@@ -91,9 +97,10 @@ const steps = function (sceneElement, modConfig, sceneConfig) {
         const els = ob.els
 
         if (!allElems[sel]) {
-          allElems[sel] = { arr: els, trans }
+          allElems[sel] = { arr: els, trans, outs: [out] }
         } else {
           allElems[sel].arr = allElems[sel].arr.concat(els)
+          allElems[sel].outs.push(out)
         }
       })
     }
@@ -101,14 +108,18 @@ const steps = function (sceneElement, modConfig, sceneConfig) {
     // end blocks
   })
 
+  // routine only for match mode
   for (const k in allElems) {
     const trans = allElems[k].trans
+    const outs = allElems[k].outs
     allElems[k].arr.forEach(el => (el.classList.add(css.stepItem, css[trans])))
 
     const id = {
       sandbox: 'steps',
       index,
       trans,
+      outs: outs,
+      out: outs.find(d => d === true),
       els: allElems[k].arr
     }
     sceneConfig._steps.push(id)
@@ -118,8 +129,19 @@ const steps = function (sceneElement, modConfig, sceneConfig) {
 
   this.stepForward = step => {
     if (step.sandbox === 'steps') {
+      if (prevEls) {
+        prevEls.forEach((el, i) => {
+          const out = step.outs ? step.outs[i] : step.out
+          if (out) el.classList.add(css[step.trans])
+        })
+        prevEls = null
+      }
       const els = step.els
       els.forEach(el => (el.classList.remove(css[step.trans])))
+
+      if (step.out) {
+        prevEls = els
+      }
     }
   }
 }
